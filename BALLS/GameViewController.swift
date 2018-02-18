bd4a1127cb218a032d49ac5855072f11544fb566
ryanmaksymic <ryanmaksymic@gmail.com>
2018-02-18 00:05:55 -0500

    Cleaned up boilerplate code

--- BALLS/GameViewController.swift
+++ BALLS/GameViewController.swift
@@ -10,56 +10,58 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
-class GameViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class GameViewController: UIViewController
+{
+  override func viewDidLoad()
+  {
+    super.viewDidLoad()
+    
+    // Load 'GameScene.sks' as a GKScene:
+    if let scene = GKScene(fileNamed: "GameScene")
+    {
+      // Get the SKScene from the loaded GKScene:
+      if let sceneNode = scene.rootNode as! GameScene?
+      {
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
+        // Copy gameplay related content over to the scene:
+        sceneNode.entities = scene.entities
+        sceneNode.graphs = scene.graphs
+        
+        // Set the scale mode to scale to fit the window:
+        sceneNode.scaleMode = .aspectFill
+        
+        // Present the scene:
+        if let view = self.view as! SKView?
+        {
+          view.presentScene(sceneNode)
+          
+          view.ignoresSiblingOrder = true
+          
+          view.showsFPS = true
+          view.showsNodeCount = true
+          view.showsPhysics = true
         }
+      }
     }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+  }
+  
+  override var shouldAutorotate: Bool
+  {
+    return true
+  }
+  
+  override var supportedInterfaceOrientations: UIInterfaceOrientationMask
+  {
+    return .portrait
+  }
+  
+  override func didReceiveMemoryWarning()
+  {
+    super.didReceiveMemoryWarning()
+  }
+  
+  override var prefersStatusBarHidden: Bool
+  {
+    return true
+  }
 }
